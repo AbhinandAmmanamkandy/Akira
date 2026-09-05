740f9d05b93afd3f124dfd637d391c5caa5032b0
AbhinandAmmanamkandy <abhinandammanamkandypvt@gmail.com>
2026-09-05 17:21:01 +0530

    Drop unwanted screens and remove navbar

--- lib/features/main/screens/main_screen.dart
+++ lib/features/main/screens/main_screen.dart
@@ -1,56 +1,11 @@
 import 'package:flutter/material.dart';
-import '../../explore/screens/explore_screen.dart';
 import '../../home/screens/home_screen.dart';
-import '../../search/screens/search_screen.dart';
 
-class MainScreen extends StatefulWidget {
+class MainScreen extends StatelessWidget {
   const MainScreen({super.key});
 
   @override
-  State<MainScreen> createState() => _MainScreenState();
-}
-
-class _MainScreenState extends State<MainScreen> {
-  int _currentIndex = 0;
-
-  final List<Widget> _screens = const [
-    HomeScreen(),
-    ExploreScreen(),
-    SearchScreen(),
-  ];
-
-  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: IndexedStack(
-        index: _currentIndex,
-        children: _screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _currentIndex,
-        onTap: (index) {
-          setState(() {
-            _currentIndex = index;
-          });
-        },
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_outlined),
-            activeIcon: Icon(Icons.home_rounded),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.explore_outlined),
-            activeIcon: Icon(Icons.explore_rounded),
-            label: 'Explore',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search_outlined),
-            activeIcon: Icon(Icons.search_rounded),
-            label: 'Search',
-          ),
-        ],
-      ),
-    );
+    return const HomeScreen();
   }
 }
